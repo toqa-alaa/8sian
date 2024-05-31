616bf95cecafc85b6f090e088902d4a2087d65a1
toqa-alaa <toqahabiba261@gmail.com>
2024-05-31 03:24:08 +0300

    last commit

---
+++ main.js
@@ -0,0 +1,17 @@
+function updateCountdown() {
+  var targetDate = new Date("June 29, 2024 15:00:00 GMT+0900").getTime();
+  var now = new Date().getTime();
+  var distance = targetDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+}
+
+setInterval(updateCountdown, 1000);
